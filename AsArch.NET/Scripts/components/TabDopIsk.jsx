dfc19b2fbd2909a81dc3b057ad83addd48e7a167
romashka77 <romashka_77@mail.ru>
2018-10-26 12:41:33 +0300

    ClearSearchButton  выходит за таблицу

--- AsArch.NET/Scripts/components/TabDopIsk.jsx
+++ AsArch.NET/Scripts/components/TabDopIsk.jsx
@@ -1,34 +1,5 @@
 ﻿var React = require('react');
-import { BootstrapTable, TableHeaderColumn, InsertButton, InsertModalFooter} from 'react-bootstrap-table';
-
-const cellEditProp = {
-    mode: 'click',
-    blurToSave: true
-};
-// validator function pass the user input value and should return true|false.
-function jobNameValidator(value) {
-    const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
-    if (!value) {
-        response.isValid = false;
-        response.notification.type = 'error';
-        response.notification.msg = 'Value must be inserted';
-        response.notification.title = 'Requested Value';
-    } else if (value.length < 10) {
-        response.isValid = false;
-        response.notification.type = 'error';
-        response.notification.msg = 'Value must have 10+ characters';
-        response.notification.title = 'Invalid Value';
-    }
-    return response;
-}
-
-function jobStatusValidator(value) {
-    const nan = isNaN(parseInt(value, 10));
-    if (nan) {
-        return 'Job Status must be a integer!';
-    }
-    return true;
-}
+import { BootstrapTable, TableHeaderColumn, InsertButton, DeleteButton, InsertModalHeader, InsertModalFooter, SearchField, ClearSearchButton } from 'react-bootstrap-table';
 
 export default class TabDopIsk extends React.Component {
     constructor(props) {
@@ -37,72 +8,71 @@ export default class TabDopIsk extends React.Component {
         this.handleDopPredIskSubmit = this.handleDopPredIskSubmit.bind(this);
     }
 
-    //invalidJobStatus = (cell, row) => {
-    //    //console.log(`${cell} at row id: ${row.id} fails on editing`);
-    //    return 'invalid-jobstatus-class';
-    //}
-    //editingJobStatus = (cell, row) => {
-    //    //console.log(`${cell} at row id: ${row.id} in current editing`);
-    //    return 'editing-jobstatus-class';
-    //}
-    //кнопка Добавить
-    handleInsertButtonClick = (onClick) => {
-        // Пользовательское событие onClick здесь, нет необходимости реализовывать эту функцию, если у вас нет никакого процесса перед onClick
-        //console.log('This is my custom function for InserButton click event');
-        onClick();
+    //DeleteButton
+    customConfirm(next, dropRowKeys) {
+        const dropRowKeysStr = dropRowKeys.join(',');
+        if (confirm(`Вы уверены, что хотите удалить ${dropRowKeysStr}?`)) {
+            //Если подтверждение истинно, вызовите функцию, которая продолжает удаление записи.
+            next();
+        }
     }
-    createCustomInsertButton = (onClick) => {
+    createCustomDeleteButton = () => {
+        return (
+            <DeleteButton
+                btnText='Удалить'
+                btnContextual='btn btn-default'
+            />
+        );
+    }
+
+    //InsertButton
+    createCustomInsertButton = () => {
         return (
             <InsertButton
                 btnText='Добавить'
                 btnContextual='btn btn-default'
-                className='my-custom-class'
-                btnGlyphicon='glyphicon-edit'
-                onClick={() => this.handleInsertButtonClick(onClick)}
             />
         );
     }
+
+    //CustomModalHeader
+    createCustomModalHeader = (closeModal, save) => {
+        return (
+            <InsertModalHeader
+                title='Добавить иск'
+            />
+        );
+    }
+
     //InsertModalFooter
-    beforeClose(e) {
-        alert(`[Custom Event]: Modal close event triggered!`);
-    }
-
-    beforeSave(e) {
-        alert(`[Custom Event]: Modal save event triggered!`);
-    }
-
-    handleModalClose(closeModal) {
-        // Custom your onCloseModal event here,
-        // it's not necessary to implement this function if you have no any process before modal close
-        console.log('This is my custom function for modal close event');
-        closeModal();
-    }
-
-    handleSave(save) {
-        // Custom your onSave event here,
-        // it's not necessary to implement this function if you have no any process before save
-        console.log('This is my custom function for save event');
-        save();
-    }
-
-    createCustomModalFooter = (closeModal, save) => {
+    createCustomModalFooter = () => {
         return (
             <InsertModalFooter
-                className='my-custom-class'
                 saveBtnText='Сохранить'
                 closeBtnText='Отмена'
                 closeBtnContextual='btn btn-default'
                 saveBtnContextual='btn btn-default'
-                closeBtnClass='my-close-btn-class'
-                saveBtnClass='my-save-btn-class'
-                beforeClose={this.beforeClose}
-                beforeSave={this.beforeSave}
-                onModalClose={() => this.handleModalClose(closeModal)}
-                onSave={() => this.handleSave(save)}
             />
         );
     }
-
+    //SearchField
+    createCustomSearchField = () => {
+        return (
+            <SearchField
+                //defaultValue='2000'
+                placeholder='Поиск...'
+            />
+        );
+    }
+    //ClearSearchButton 
+    createCustomClearButton = () => {
+        return (
+            <ClearSearchButton
+                btnText='Очистить'
+                btnContextual='btn btn-default'
+            />
+        );
+    }
 
     //вызывается после рендеринга компонента. Здесь можно выполнять запросы к удаленным ресурсам
     componentDidMount() {
@@ -145,10 +115,24 @@ export default class TabDopIsk extends React.Component {
     render() {
         const options = {
             insertBtn: this.createCustomInsertButton,
-            insertModalFooter: this.createCustomModalFooter
+            deleteBtn: this.createCustomDeleteButton,
+            handleConfirmDeleteRow: this.customConfirm,
+            insertModalHeader: this.createCustomModalHeader,
+            insertModalFooter: this.createCustomModalFooter,
+            searchField: this.createCustomSearchField,
+            clearSearch: true,
+            clearSearchBtn: this.createCustomClearButton
         };
         return (
-            <BootstrapTable data={this.state.data} cellEdit={cellEditProp} options={options} insertRow>
+            <BootstrapTable
+                data={this.state.data}
+                cellEdit={{ mode: 'click', blurToSave: true }}
+                selectRow={{ mode: 'checkbox' }}
+                options={options}
+                insertRow
+                deleteRow
+                search
+            >
                 <TableHeaderColumn isKey={true} dataField='Id'>№</TableHeaderColumn>
                 <TableHeaderColumn dataField='Name' editable={{ type: 'select', options: { values: this.state.options } }}>Сопутствующий предмет иска</TableHeaderColumn>
                 <TableHeaderColumn dataField='Comment' editable={{ type: 'textarea' }}>Примечание</TableHeaderColumn>

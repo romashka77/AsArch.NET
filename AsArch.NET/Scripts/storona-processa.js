dc846527d728917706daea00ba2974f3123a381f
romashka77 <romashka_77@mail.ru>
2018-11-08 16:49:30 +0300

    $(fiz_lico).hide({ duration: 800, easing: "linear", queue: false });

--- AsArch.NET/Scripts/storona-processa.js
+++ AsArch.NET/Scripts/storona-processa.js
@@ -3,14 +3,14 @@
     let fiz_lico = '.fiz-lico';
     let ur_lico = '.ur-lico';
     if (this.value === 'Физическое лицо') {
-        $(fiz_lico).show();
-        $(ur_lico).hide();
+        $(fiz_lico).show({ duration: 800, easing: "linear", queue: false });
+        $(ur_lico).hide({ duration: 800, easing: "linear", queue: false });
     } else if (this.value === 'Юридическое лицо') {
-        $(ur_lico).show();
-        $(fiz_lico).hide();
+        $(ur_lico).show({ duration: 800, easing: "linear", queue: false });
+        $(fiz_lico).hide({ duration: 800, easing: "linear", queue: false });
     } else {
-        $(fiz_lico).hide();
-        $(ur_lico).hide();
+        $(fiz_lico).hide({ duration: 800, easing: "linear", queue: false });
+        $(ur_lico).hide({ duration: 800, easing: "linear", queue: false });
     }
 });
 
